1c9eb64d7f94cbd291b5d4cb0c38bce4bfb73ea8
timjolson <tim.lsn@gmail.com>
2019-05-29 05:34:15 -0500

    MPRunner: Improved logging, registering. MPStreamHandler: Moved identifier out of createLock. MPFileHandler: Automatic make directories, improved lock key, starts lockserver if not running.

--- mp_utils.py
+++ mp_utils.py
@@ -1,5 +1,5 @@
 import logging
-from multiprocessing import current_process, managers, Process, active_children as mpactive_children
+from multiprocessing import current_process, managers, Process
 from subprocess import Popen
 import threading
 import queue
@@ -13,6 +13,33 @@ logger.addHandler(logging.NullHandler())
 
 class AcquirerProxy(managers.AcquirerProxy):
     _exposed_ = ('acquire', 'release', '__enter__', '__exit__')
+
+
+if 'redis' in sys.modules:
+    import redis
+    class RedisStorage(redis.Redis):
+        access = 'client'
+        getLock = redis.Redis.lock
+        getRLock = redis.Redis.lock
+
+        def start(self):
+            return self
+        connect = start
+
+        # make proxies
+        # getValue =
+        # getList =
+        # getQueue =
+        # getJoinableQueue =
+        # getEvent =
+        # getSemaphore =
+        # getBoundedSemaphore =
+        # getCondition =
+        # getBarrier =
+        # getPool =
+        # getDict =
+        # getArray =
+        # getNamespace =
 
 
 class MPStorage(managers.SyncManager):
@@ -99,15 +126,16 @@ class MPStorage(managers.SyncManager):
         )
 
         self.logger = logging.getLogger(
-            '.'.join([__name__, self.__class__.__name__, str(self.address)]))
+            '.'.join([__name__, type(self).__name__]) + ':' + str(self.address)
+        )
         self.logger.addHandler(logging.NullHandler())
 
     def _getitem(self, key, _type, *args, **kwargs):
         if _type not in self._storage.keys():
-            self.logger.info(f"Creating dict for type {_type}")
+            self.logger.info(f"Creating dict for type {_type.__name__}")
             self._storage[_type] = dict()
         if key not in self._storage[_type].keys():
-            self.logger.info(f"Creating {_type} for key '{key}'")
+            self.logger.info(f"Creating {_type.__name__} for key '{key}'")
             item = _type(*args, **kwargs)
             self._storage[_type][key] = item
         return self._storage[_type][key]
@@ -136,71 +164,128 @@ class MPRunner(object):
     """
     # https://pymotw.com/2/multiprocessing/basics.html
     """
+    logger = logging.getLogger('.'.join([__name__, 'MPRunner']))
+    logger.addHandler(logging.NullHandler())
 
-    def __init__(self):
+    def __init__(self, procs=None):
         self.procs = defaultdict(list)
         self._allprocs = []
         self._active = []
 
-        self.logger = logging.getLogger(
-            '.'.join([__name__, self.__class__.__name__, str(self.address)]))
-        self.logger.addHandler(logging.NullHandler())
+        if procs is not None:
+            self.register_multiple(procs)
+
+    def register_multiple(self, procs):
+        if isinstance(procs, dict):
+            for k, v in procs.items():
+                if v in [None, False, (), {}, []]:
+                    self.register(k)
+                    continue
+
+                if isinstance(v, dict):
+                    self.register(k, kwargs=v)
+                    continue
+
+                if isinstance(v, bool):
+                    self.register(k, daemon=v)
+                    continue
+
+                v = list(v)
+                daemon, args, kwargs = False, None, None
+                for item in v:
+                    if isinstance(item, bool):
+                        daemon = item
+                    elif isinstance(item, dict):
+                        kwargs = item
+                    else:
+                        args = item
+                self.logger.warning(("register_multiple()", k, daemon, args, kwargs))
+
+                self.register(k, daemon, args, kwargs)
+        elif isinstance(procs, (list, tuple)) or hasattr(procs, '__iter__') or hasattr(procs, '__next__'):
+            for p in procs:
+                self.register(p)
+        elif callable(procs) or isinstance(procs, str):
+            self.register(procs)
+        else:
+            raise ValueError(f"Provide dict, not {type(procs)}")
 
     def process_start_callback(self, proc):
-        self.logger.debug(f"Starting {'daemon' if proc.daemon else ''} '{proc.name}' at {proc.pid}")
+        self.logger.info(f"Starting {'daemon ' if proc.daemon else ''}'{proc}' at pid {proc.pid}")
 
     def process_end_callback(self, proc):
-        self.logger.debug(f"Finished process {proc.pid}: '{proc.name}'")
+        self.logger.info(f"Finished process '{proc}' at pid {proc.pid}")
 
-    def register_process(self, process, daemon=False, *args, **kwargs):
-        Process.daemon = daemon
+    def register_process(self, process, daemon=None):
+        if daemon is not None:
+            process.daemon = daemon
         self.procs[process.name].append(process)
         self._allprocs.append(process)
         self.logger.info(f"Registered process '{process.name}'")
-        return Process
+        return process
 
-    def register_function(self, func, daemon=False, *args, **kwargs):
+    def register_function(self, func, daemon=False, args=None, kwargs=None):
         name = getattr(func, '__name__', "Function")
-        pr = Process(name, target=func, daemon=daemon,
-                     args=args, kwargs=kwargs)
+
+        pr = self._make_process(name, func, daemon, args, kwargs)
+
         self.procs[pr.name].append(pr)
         self._allprocs.append(pr)
-        self.logger.info(f"Registered function '{pr.name}'")
+        self.logger.info(f"Registered function '{pr.name}' {daemon} {args} {kwargs}")
         return pr
 
-    def register_command(self, cmds, daemon=False, *args, **kwargs):
+    def register_command(self, cmds, daemon=False, args=None, kwargs=None):
         if not isinstance(cmds, str):
             cmds = [c for c in cmds]
             if not all(isinstance(c, str) for c in cmds):
                 raise ValueError(f"Commands must all be strings")
             cmds = ' '.join(cmds)
 
-        pr = Process(
-            name=cmds, daemon=daemon,
-            target=lambda *a, **kwargs: Popen(cmds, *a, **kwargs), args=args, kwargs=kwargs
-        )
+        pr = self._make_process(cmds, Popen, daemon, args, kwargs)
+
         self.procs[cmds].append(pr)
         self._allprocs.append(pr)
         self.logger.info(f"Registered command '{pr.name}'")
         return pr
 
-    def register(self, process, daemon=False, *args, **kwargs):
+    def _make_process(self, name, func, daemon=False, args=None, kwargs=None):
+        args = () if args is None else args
+        kwargs = {} if kwargs is None else kwargs
+
+        if args and kwargs:
+            return Process(name=name, daemon=daemon, target=func, args=args, kwargs=kwargs)
+        elif args:
+            return Process(name=name, daemon=daemon, target=func, args=args)
+        elif kwargs:
+            return Process(name=name, daemon=daemon, target=func, kwargs=kwargs)
+        else:
+            return Process(name=name, daemon=daemon, target=func)
+
+    def register(self, process, daemon=False, args=None, kwargs=None):
         if isinstance(process, Process):
-            pr = self.register_process(Process, daemon=daemon, *args, **kwargs)
+            return self.register_process(Process, daemon)
         elif callable(process):
-            pr = self.register_function(process, daemon=daemon, *args, **kwargs)
+            return self.register_function(process, daemon, args, kwargs)
         elif isinstance(process, (str, list, tuple)) or hasattr(process, '__iter__') or hasattr(process, '__next__'):
-            pr = self.register_command(process, daemon=daemon, *args, **kwargs)
+            return self.register_command(process, daemon, args, kwargs)
         else:
             raise NotImplementedError(f"Cannot handle {process}")
-        return pr
 
     def run(self):
         self.logger.info(f"Starting {sum(len(p) for p in self.procs.values())} processes.")
-        for name, pr in self.procs.items():
-            pr.start()
-            self._active.append(pr)
-            self.process_start_callback(pr)
+        for name, prs in self.procs.items():
+            for pr in prs:
+                self.process_start_callback(pr)
+                pr.run()
+                self.process_end_callback(pr)
+
+    def start(self):
+        self.logger.info(f"Starting {sum(len(p) for p in self.procs.values())} processes.")
+        for name, prs in self.procs.items():
+            for pr in prs:
+                self._active.append(pr)
+                pr.start()
+                self.process_start_callback(pr)
 
     def shutdown(self, wait_for_daemons=True):
         ps = list(self._allprocs)
@@ -222,10 +307,11 @@ class MPRunner(object):
         self.monitor()
         self.logger.info(f"{type(self).__name__} has shutdown.")
 
-    def join(self):
+    def join(self, timeout=None):
         self.logger.debug("Joining processes")
         for p in self._allprocs:
-            p.join()
+            p.join(timeout)
+            self.logger.debug(f"joined {p}")
 
     def active_children(self):
         return [p for p in self._allprocs if p.is_alive()]
@@ -255,57 +341,68 @@ class MPRunner(object):
 if 'redis' in sys.modules:
     import redis
     address = ('localhost', 6379)
-    _server = redis.Redis(host=address[0], port=address[1])
-    _server.ping()
-    _getlock = lambda n, *x, **k: _server.lock(str(n), *x, **k)
+    lockserver = redis.Redis(host=address[0], port=address[1])
+    lockserver.ping()
+    _getlock = lockserver.lock
 else:
     address = ('localhost', 12345)
     authkey = b'localhost:12345.__mp_log_handlers'
-    _server = MPStorage(address, authkey=authkey)
-    _getlock = lambda n, *x, **k: _server.getRLock(str(n), *x, **k)
+    lockserver = MPStorage(address, authkey=authkey)
+    _getlock = lockserver.getRLock
+
+
+def _get_stream_identifier(stream):
+    def get_name(stm):
+        if hasattr(stm, 'buffer'):
+            buffer = stm.buffer
+            if hasattr(buffer, 'raw'):
+                raw = buffer.raw
+                if hasattr(raw, 'name'):
+                    if isinstance(raw.name, str):
+                        return raw.name
+            if hasattr(buffer, 'name'):
+                if isinstance(buffer.name, (str, int)):
+                    return buffer.name
+        if hasattr(stm, 'name'):
+            if isinstance(stm.name, (str, int)):
+                return stm.name
+        return None
+
+    key = get_name(stream)
+    try: key = (current_process().pid, int(key))
+    except: pass
+
+    if isinstance(key, str):
+        key = os.path.abspath(key)
+
+    if key is None:
+        key = (current_process().pid, str(stream))
+
+    return str(key)
 
 
 class MPStreamHandler(logging.StreamHandler):
-    def __init__(self, stream=None):
-        if isinstance(_server, MPStorage):
-            if _server._state.value == managers.State.INITIAL:
-                _server.start()
+    # logger = logging.getLogger('.'.join([__name__, 'MPStreamHandler']))
+    # logger.addHandler(logging.NullHandler())
 
+    def __init__(self, stream=None):
         if stream is None:
             stream = sys.stderr
         self.stream = stream
         logging.Handler.__init__(self)
 
+    def lock_factory(self, identifier):
+        return _getlock(identifier)
+
     def createLock(self):
-        def get_name(stm):
-            if hasattr(stm, 'buffer'):
-                buffer = stm.buffer
-                if hasattr(buffer, 'raw'):
-                    raw = buffer.raw
-                    if hasattr(raw, 'name'):
-                        if isinstance(raw.name, str):
-                            return raw.name
-                if hasattr(buffer, 'name'):
-                    if isinstance(buffer.name, (str, int)):
-                        return buffer.name
-            if hasattr(stm, 'name'):
-                if isinstance(stm.name, (str, int)):
-                    return stm.name
-            return None
+        if isinstance(lockserver, MPStorage):
+            if lockserver._state.value == managers.State.INITIAL:
+                lockserver.start()
 
-        key = get_name(self.stream)
-        try: key = (current_process().pid, int(key))
-        except: pass
-
-        if isinstance(key, str):
-            key = os.path.abspath(key)
-
-        if key is None:
-            key = (current_process().pid, str(self.stream))
-
-        logger.debug(f".MPStreamHandler:{self.stream} makes key {key}")
-
-        self.lock = _getlock(key)
+        key = '.'.join([__name__, type(self).__name__]) + ':' + _get_stream_identifier(self.stream)
+        self.lock_name = key
+        # self.logger.debug(f"Made key '{key}' for {self.stream}")
+        self.lock = self.lock_factory(key)
 
     def flush(self):
         if self.stream and hasattr(self.stream, "flush"):
@@ -330,6 +427,9 @@ class MPStreamHandler(logging.StreamHandler):
 
 
 class MPFileHandler(MPStreamHandler):
+    # logger = logging.getLogger('.'.join([__name__, 'MPFileHandler']))
+    # logger.addHandler(logging.NullHandler())
+
     def __init__(self, filename, mode='a', encoding=None, delay=False):
         filename = os.fspath(filename)
 
@@ -343,14 +443,23 @@ class MPFileHandler(MPStreamHandler):
         else:
             MPStreamHandler.__init__(self, self._open())
 
+        filepath = os.path.dirname(self.baseFilename)
+        os.makedirs(filepath, exist_ok=True)
+
     def _open(self):
         if not os.path.exists(os.path.dirname(self.baseFilename)):
             os.makedirs(os.path.dirname(self.baseFilename))
         return open(self.baseFilename, self.mode, encoding=self.encoding)
 
     def createLock(self):
-        key = self.baseFilename
-        self.lock = _getlock(key)
+        if isinstance(lockserver, MPStorage):
+            if lockserver._state.value == managers.State.INITIAL:
+                lockserver.start()
+
+        key = '.'.join([__name__, type(self).__name__]) + ':' + self.baseFilename
+        self.lock_name = key
+        # self.logger.debug(f"Made key '{key}' for {self.stream}")
+        self.lock = self.lock_factory(key)
 
     def emit(self, record):
         try:
@@ -367,3 +476,5 @@ class MPFileHandler(MPStreamHandler):
 
 
 __all__ = ['MPStreamHandler', 'MPFileHandler', 'MPStorage', 'MPRunner']
+if 'redis' in sys.modules:
+    __all__.append('RedisStorage')
